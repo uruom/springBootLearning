e4071b438e283edfe4a927e1328dfb840179a5d8
uruom <2268074651@qq.com>
2023-07-11 10:53:12 +0800

    P10

---
+++ src/main/java/com/uruom/springbooot02config/Springbooot02ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.uruom.springbooot02config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbooot02ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbooot02ConfigApplication.class, args);
+    }
+
+}
