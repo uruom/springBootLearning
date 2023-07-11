3c2f2bafd3aa844efe973867a32cd5f77441d6cd
uruom <2268074651@qq.com>
2023-07-11 11:18:50 +0800

    application

--- src/test/java/com/uruom/springbooot02config/Springbooot02ConfigApplicationTests.java
+++ src/test/java/com/uruom/springbooot02config/Springbooot02ConfigApplicationTests.java
@@ -1,20 +1,10 @@
 package com.uruom.springbooot02config;
 
-import com.uruom.springbooot02config.pojo.Dog;
-import com.uruom.springbooot02config.pojo.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springbooot02ConfigApplicationTests {
-
-    @Autowired
-    private Person person;
-
-    @Test
-    void contextLoads() {
-        System.out.println(person);
-    }
 
 }
